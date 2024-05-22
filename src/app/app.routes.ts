dfb3dc7031e1ae32296564985f2233c1d27d3352
Pierre Viara <pierre.viara@outlook.com>
2024-05-22 10:12:11 +0200

    🎁 add landing page first version (#4)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
         path: '',
-        component: HomeComponent,
+        component: LandingComponent,
     },
+    {
+        path: 'home',
+        component: HomeComponent,
+    }
 ];

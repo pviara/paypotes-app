be39d3c75c1914b0a25b3aaaf56ab56ba6e116c0
Pierre Viara <pierre.viara@outlook.com>
2024-05-30 13:22:11 +0200

    isolate skeleton in single component

--- src/app/shared/expense/expense.component-module.ts
+++ src/app/shared/expense/expense.component-module.ts
@@ -2,10 +2,16 @@ import { CommonModule } from '@angular/common';
 import { DescriptionComponent } from '@shared/expense/description/description.component';
 import { ExpenseComponent } from '@shared/expense/expense.component';
 import { NgModule } from '@angular/core';
+import { SkeletonComponent } from '@shared/expense/skeleton/skeleton.component';
 import { TotalComponent } from '@shared/expense/total/total.component';
 
 @NgModule({
-    declarations: [DescriptionComponent, ExpenseComponent, TotalComponent],
+    declarations: [
+        DescriptionComponent,
+        ExpenseComponent,
+        SkeletonComponent,
+        TotalComponent,
+    ],
     exports: [ExpenseComponent],
     imports: [CommonModule],
 })

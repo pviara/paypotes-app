33b3ef929fdf367981ace68a7ecd4188a4e36645
Pierre Viara <pierre.viara@outlook.com>
2024-06-02 17:36:46 +0200

    move list and filters to explorer folder

--- src/app/shared/explorer/explorer.component-module.ts
+++ src/app/shared/explorer/explorer.component-module.ts
@@ -1,6 +1,6 @@
 import { ExplorerComponent } from '@shared/explorer/explorer.component';
-import { FiltersComponent } from '@shared/filters/filters.component';
-import { ListComponentModule } from '@shared/list/list.component-module';
+import { FiltersComponent } from '@shared/explorer/filters/filters.component';
+import { ListComponentModule } from '@shared/explorer/list/list.component-module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 

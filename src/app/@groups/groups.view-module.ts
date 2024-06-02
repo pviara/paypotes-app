4d20a79f36e496819829149b9752805be74b206b
Pierre Viara <pierre.viara@outlook.com>
2024-06-02 17:27:45 +0200

    add explorer component and move group

--- src/app/@groups/groups.view-module.ts
+++ src/app/@groups/groups.view-module.ts
@@ -1,18 +1,20 @@
+import { ExplorerComponentModule } from '@shared/explorer/explorer.component-module';
+import { FiltersComponent } from '@shared/filters/filters.component';
 import { HeadbarComponent } from '@shared/headbar/headbar.component';
 import { GroupsComponent } from '@groups/groups/groups.component';
 import { groupsRoutes } from '@groups/groups.routes';
 import { GroupsView } from '@groups/groups.view';
+import { ListComponentModule } from '@shared/list/list.component-module';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CoreModule } from '@core/core.module';
-import { FiltersComponent } from '@shared/filters/filters.component';
-import { ListComponentModule } from '@shared/list/list.component-module';
 
 @NgModule({
     declarations: [GroupsComponent, GroupsView],
     exports: [RouterModule],
     imports: [
         CoreModule,
+        ExplorerComponentModule,
         FiltersComponent,
         HeadbarComponent,
         ListComponentModule,
--- src/app/@groups/groups/groups.component.ts
+++ src/app/@groups/groups/groups.component.ts
@@ -13,100 +13,16 @@ import { ListElements } from '@core/model/list-element/list-element';
 export class GroupsComponent {
     private groupService = inject(GroupServiceToken);
 
-    private lastFetchedGroupsCount = 0;
-
-    private nextIndex = 0;
-
-    private savedFilters?: Filters;
-
-    private skeletons: Array<null> = Array.from({ length: 20 }).map(() => null);
-
     $groups = new BehaviorSubject<ListElements>([]);
 
-    filtering = false;
-
-    ngOnInit(): void {
-        this.prepareList();
-        this.getGroups();
-    }
-
-    onGroupHovered(groupId: string): void {
-        if (
-            this.isGroupNearListEnd(groupId) &&
-            this.lastFetchedGroupsCount === 20
-        ) {
-            this.addSkeletonsToList();
-            this.getNextPageGroups();
-        }
-    }
-
-    onUpdatedFilters(filters: Filters): void {
-        this.filtering = true;
-        this.resetNextIndex();
-        this.saveFilters(filters);
-        this.prepareList();
-
+    onElementsRequested(event: {
+        pageIndex?: number;
+        filters?: Filters;
+    }): void {
         this.groupService
-            .getGroups(this.nextIndex, filters)
+            .getGroups(event.pageIndex, event.filters)
             .subscribe((groups) => {
                 this.$groups.next(groups);
-                this.filtering = false;
             });
-    }
-
-    private prepareList(): void {
-        this.emptyList();
-        this.addSkeletonsToList();
-    }
-
-    private addSkeletonsToList(): void {
-        const newGroups = this.$groups.getValue().concat(this.skeletons);
-        this.$groups.next(newGroups);
-    }
-
-    private getGroups(): void {
-        this.groupService
-            .getGroups(this.nextIndex, this.savedFilters)
-            .pipe(
-                tap((groups) => (this.lastFetchedGroupsCount = groups.length)),
-            )
-            .subscribe(this.appendGroupsToList());
-    }
-
-    private getNextPageGroups(): void {
-        this.nextIndex++;
-        this.getGroups();
-    }
-
-    private appendGroupsToList(): (groups: Groups) => void {
-        return (newGroups: Groups) => {
-            let groups = this.$groups.getValue().filter((group) => !!group);
-            groups = groups.concat(newGroups);
-            this.$groups.next(groups);
-        };
-    }
-
-    private isGroupNearListEnd(groupId: string): boolean {
-        const index = this.findGroupIndexWith(groupId);
-        const isNearArrayEnd = index > this.$groups.getValue().length - 10;
-        return isNearArrayEnd;
-    }
-
-    private findGroupIndexWith(groupId: string): number {
-        return this.$groups
-            .getValue()
-            .findIndex((group) => group?.getId() === groupId);
-    }
-
-    private resetNextIndex(): void {
-        this.nextIndex = 0;
-    }
-
-    private saveFilters(filters: Filters): void {
-        this.savedFilters = filters;
-    }
-
-    private emptyList(): void {
-        this.$groups.next([]);
     }
 }

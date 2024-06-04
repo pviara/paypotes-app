f1e5c20df56881fff607aa8e1f77222bf5df7d9c
Pierre Viara <pierre.viara@outlook.com>
2024-06-04 12:11:12 +0200

    isolate components into modules

---
+++ src/app/@expenses/expense/expense.component-module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { CoreModule } from '@core/core.module';
+import { ExpenseDescriptionComponent } from '@expenses/expense/description/description.component';
+import { ExpenseHeaderComponent } from '@expenses/expense/header/header.component';
+import { ExpenseImagesComponent } from '@expenses/expense/images/images.component';
+import { ExpenseComponent } from '@expenses/expense/expense.component';
+import { ExpenseSkeletonComponent } from '@expenses/expense/skeleton/skeleton.component';
+import { NgModule } from '@angular/core';
+import { SharedModule } from '@shared/shared.module';
+
+@NgModule({
+    declarations: [
+        ExpenseDescriptionComponent,
+        ExpenseHeaderComponent,
+        ExpenseImagesComponent,
+        ExpenseSkeletonComponent,
+        ExpenseComponent,
+    ],
+    imports: [CommonModule, CoreModule, SharedModule],
+})
+export class ExpenseComponentModule {}
--- src/app/@expenses/expenses.view-module.ts
+++ src/app/@expenses/expenses.view-module.ts
@@ -1,37 +1,19 @@
-import { CommonModule } from '@angular/common';
-import { CoreModule } from '@core/core.module';
-import { ExpenseComponent } from '@expenses/expense/expense.component';
-import { ExpenseDescriptionComponent } from '@expenses/expense/description/description.component';
-import { ExpenseHeaderComponent } from '@expenses/expense/header/header.component';
-import { ExpenseImagesComponent } from '@expenses/expense/images/images.component';
-import { ExpenseSkeletonComponent } from '@expenses/expense/skeleton/skeleton.component';
+import { ExpenseComponentModule } from '@expenses/expense/expense.component-module';
+import { ExpensesComponentModule } from '@expenses/expenses/expenses.component-module';
 import { expensesRoutes } from '@expenses/expenses.routes';
 import { ExpensesView } from '@expenses/expenses.view';
-import { ExpensesComponent } from '@expenses/expenses/expenses.component';
-import { ExplorerComponentModule } from '@shared/explorer/explorer.component-module';
 import { HeadbarComponent } from '@shared/headbar/headbar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
-    declarations: [
-        ExpenseDescriptionComponent,
-        ExpenseHeaderComponent,
-        ExpenseImagesComponent,
-        ExpenseSkeletonComponent,
-        ExpensesView,
-        ExpenseComponent,
-        ExpensesComponent,
-    ],
+    declarations: [ExpensesView],
     exports: [RouterModule],
     imports: [
-        CommonModule,
-        CoreModule,
-        ExplorerComponentModule,
+        ExpenseComponentModule,
+        ExpensesComponentModule,
         HeadbarComponent,
         RouterModule.forChild(expensesRoutes),
-        SharedModule,
     ],
 })
 export class ExpensesViewModule {}

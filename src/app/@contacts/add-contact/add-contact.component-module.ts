3035eb590a052c2dd277e1f1dcaad42fd4490450
Pierre Viara <pierre.viara@outlook.com>
2024-06-30 23:41:34 +0200

    add required component files

---
+++ src/app/@contacts/add-contact/add-contact.component-module.ts
@@ -0,0 +1,7 @@
+import { AddContactComponent } from '@contacts/add-contact/add-contact.component';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    declarations: [AddContactComponent],
+})
+export class AddContactComponentModule {}
---
+++ src/app/@contacts/add-contact/add-contact.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'add-contact',
+    templateUrl: './add-contact.component.html',
+    styleUrls: ['./add-contact.component.scss'],
+})
+export class AddContactComponent {}

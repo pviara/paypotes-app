258d78e5da5a8dff8f16bc959cca0c6aff487f4a
Pierre Viara <pierre.viara@outlook.com>
2024-08-19 10:47:25 +0200

    add a few mechanisms

--- src/app/@expenses/add-expense/add-expense.component.ts
+++ src/app/@expenses/add-expense/add-expense.component.ts
@@ -19,9 +19,48 @@ export class AddExpenseComponent implements OnInit {
 
     ngOnInit(): void {
         this.form = this.formBuilder.group({
-            balance: this.formBuilder.nonNullable.control('00,00'),
+            balance: this.formBuilder.nonNullable.control(''),
             name: this.formBuilder.nonNullable.control(''),
             isCurrentPayer: this.formBuilder.nonNullable.control(false),
         });
+    }
+
+    onBalanceChange(event: Event): void {
+        const { value } = event.target as HTMLInputElement;
+        if (!value) {
+            return;
+        }
+
+        const balance = this.removeInvalidCharacters(value);
+        this.form.controls.balance.setValue(balance, { emitEvent: false });
+    }
+
+    private removeInvalidCharacters(value: string): string {
+        if (value === ',') {
+            return '';
+        }
+
+        const cleanedValue = value.replaceAll(
+            /[a-zA-Z\u00C0-\u00FF\W\s&]/g,
+            (match) => (match === ',' ? ',' : ''),
+        );
+
+        return this.containsExtraComma(cleanedValue)
+            ? this.removeExtraCommaFrom(cleanedValue)
+            : cleanedValue;
+    }
+
+    private containsExtraComma(cleanedValue: string): boolean {
+        return (
+            Array.from(cleanedValue).filter((char) => char === ',').length > 1
+        );
+    }
+
+    private removeExtraCommaFrom(cleanedValue: string): string {
+        const lastCommaIndex = cleanedValue.lastIndexOf(',');
+        return Array.from(cleanedValue)
+            .map((char, index) => (index === lastCommaIndex ? '' : char))
+            .join('')
+            .toString();
     }
 }

fbc0c7095d3b87dde3e1e027d53e0e656aa631bf
Pierre Viara <pierre.viara@outlook.com>
2024-08-20 12:03:52 +0200

    add info form component

--- src/app/@expenses/add-expense/add-expense.component-module.ts
+++ src/app/@expenses/add-expense/add-expense.component-module.ts
@@ -1,11 +1,17 @@
 import { AddExpenseComponent } from '@expenses/add-expense/add-expense.component';
 import { BalanceComponentModule } from '@expenses/add-expense/balance/balance.component-module';
 import { CommonModule } from '@angular/common';
+import { InfoComponentModule } from '@expenses/add-expense/info/info-component.module';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [AddExpenseComponent],
-    imports: [BalanceComponentModule, CommonModule, ReactiveFormsModule],
+    imports: [
+        BalanceComponentModule,
+        CommonModule,
+        InfoComponentModule,
+        ReactiveFormsModule,
+    ],
 })
 export class AddExpenseComponentModule {}
---
+++ src/app/@expenses/add-expense/info/info-component.module.ts
@@ -0,0 +1,8 @@
+import { InfoComponent } from '@expenses/add-expense/info/info.component';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    declarations: [InfoComponent],
+    exports: [InfoComponent],
+})
+export class InfoComponentModule {}

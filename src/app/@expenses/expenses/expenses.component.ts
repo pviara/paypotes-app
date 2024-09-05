c999744936ebd297969060a8c613367f3496fa18
Pierre Viara <pierre.viara@outlook.com>
2024-09-05 12:24:33 +0200

    fix injection problems for expenses view

--- src/app/@expenses/expenses/expenses.component.ts
+++ src/app/@expenses/expenses/expenses.component.ts
@@ -1,11 +1,18 @@
 import { BehaviorSubject } from 'rxjs';
 import { Component, inject } from '@angular/core';
-import { ExpenseServiceToken } from '@core/services/expense/expense.service.provider';
+import { ExpenseServiceProvider, ExpenseServiceToken } from '@core/services/expense/expense.service.provider';
 import { FiltersEvent } from '@core/model/filters/filters-event';
+import { HttpClientServiceProvider } from '@core/services/http-client/http-client.service.provider';
 import { ListElements } from '@core/model/list-element/list-element';
+import { QueryServiceProvider } from '@core/services/query/query.service.provider';
 
 @Component({
     templateUrl: './expenses.component.html',
+    providers: [
+        ExpenseServiceProvider,
+        HttpClientServiceProvider,
+        QueryServiceProvider,
+    ]
 })
 export class ExpensesComponent {
     private expenseService = inject(ExpenseServiceToken);

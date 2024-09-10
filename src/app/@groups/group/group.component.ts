207ee1a62ba56bb0e3e2d06e532ee592e5559c33
Pierre Viara <pierre.viara@outlook.com>
2024-09-10 16:46:50 +0200

    edit workflows

--- src/app/@groups/group/group.component.ts
+++ src/app/@groups/group/group.component.ts
@@ -1,29 +1,15 @@
 import { ActivatedRoute } from '@angular/router';
 import { BehaviorSubject, shareReplay, switchMap, tap } from 'rxjs';
 import { Component, inject } from '@angular/core';
-import {
-    ExpenseServiceProvider,
-    ExpenseServiceToken,
-} from '@core/services/expense/expense.service.provider';
+import { ExpenseServiceToken } from '@core/services/expense/expense.service.provider';
 import { FiltersEvent } from '@core/model/filters/filters-event';
-import {
-    GroupServiceProvider,
-    GroupServiceToken,
-} from '@core/services/group/group.service.provider';
-import { HttpClientServiceProvider } from '@core/services/http-client/http-client.service.provider';
+import { GroupServiceToken } from '@core/services/group/group.service.provider';
 import { ListElements } from '@core/model/list-element/list-element';
-import { QueryServiceProvider } from '@core/services/query/query.service.provider';
 
 @Component({
     selector: 'group',
     templateUrl: './group.component.html',
     styleUrls: ['./group.component.scss'],
-    providers: [
-        ExpenseServiceProvider,
-        GroupServiceProvider,
-        HttpClientServiceProvider,
-        QueryServiceProvider,
-    ],
 })
 export class GroupComponent {
     private expenseService = inject(ExpenseServiceToken);

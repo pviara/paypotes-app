e5152ca1cf1ebbe632e73971a637524f0483022f
Pierre Viara <pierre.viara@outlook.com>
2024-11-12 11:53:34 +0100

    add set amount component and view service

--- src/app/add-expense/add-expense.view-module.ts
+++ src/app/add-expense/add-expense.view-module.ts
@@ -1,10 +1,12 @@
 import { addExpenseRoutes } from './add-expense.routes';
 import { AddExpenseView } from './add-expense.view';
+import { AddExpenseViewService } from './add-expense.view-service';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [AddExpenseView],
     imports: [RouterModule.forChild(addExpenseRoutes)],
+    providers: [AddExpenseViewService],
 })
 export class AddExpenseViewModule {}
---
+++ src/app/add-expense/set-amount/set-amount.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'set-amount',
+    templateUrl: './set-amount.component.html',
+    styleUrls: ['./set-amount.component.scss'],
+})
+export class SetAmountComponent {}

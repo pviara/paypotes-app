95b7a5eac74e70f369d2f359896b56575bd9f323
Pierre Viara <pierre.viara@outlook.com>
2024-11-13 10:53:57 +0100

    add balance displayer

--- src/app/add-expense/set-balance/set-balance.component-module.ts
+++ src/app/add-expense/set-balance/set-balance.component-module.ts
@@ -1,4 +1,5 @@
 import { ButtonComponent } from '../button/button.component';
+import { DisplayerComponent } from './displayer/displayer.component';
 import { HeadbarComponent } from '../headbar/headbar.component';
 import { KeyboardComponent } from './keyboard/keyboard.component';
 import { NgModule } from '@angular/core';
@@ -6,7 +7,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { SetBalanceComponent } from './set-balance.component';
 
 @NgModule({
-    declarations: [KeyboardComponent, SetBalanceComponent],
+    declarations: [DisplayerComponent, KeyboardComponent, SetBalanceComponent],
     imports: [ButtonComponent, HeadbarComponent, ReactiveFormsModule],
 })
 export class SetBalanceComponentModule {}

7ed71ecf7b41a929afd0a0fd425f07a3cf2a69e8
Pierre Viara <pierre.viara@outlook.com>
2024-11-13 17:09:46 +0100

    add chose contact component

--- src/app/add-expense/chose-contact/chose-contact.component.ts
+++ src/app/add-expense/chose-contact/chose-contact.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Contact } from '@core/model/contact/contact';
+import {
+    ContactServiceProvider,
+    ContactServiceToken,
+} from '@core/services/contact/contact.api-service.provider';
+import { HttpClientServiceProvider } from '@core/services/http-client/http-client.service.provider';
+import { QueryServiceProvider } from '@core/services/query/query.service.provider';
 
 @Component({
     selector: 'chose-contact',
     templateUrl: './chose-contact.component.html',
     styleUrls: ['./chose-contact.component.scss'],
+    providers: [
+        ContactServiceProvider,
+        HttpClientServiceProvider,
+        QueryServiceProvider,
+    ],
 })
-export class ChoseContactComponent {}
+export class ChoseContactComponent {
+    private contactService = inject(ContactServiceToken);
+
+    $contacts = this.contactService.getContacts();
+
+    isLastFrom(contacts: Contact[], index: number): boolean {
+        return index === contacts.length - 1;
+    }
+}
---
+++ src/app/add-expense/chose-contact/contact/contact.component.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+import { Contact } from '@core/model/contact/contact';
+
+@Component({
+    selector: 'contact',
+    templateUrl: './contact.component.html',
+    styleUrls: ['./contact.component.scss'],
+})
+export class ContactComponent {
+    contact = input.required<Contact>();
+    last = input<boolean>(false);
+}

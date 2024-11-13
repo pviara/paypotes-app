485a5c10acd90a9df404fc548a0424aa4bc9c120
Pierre Viara <pierre.viara@outlook.com>
2024-11-13 11:41:42 +0100

    add button and displayer in form

--- src/app/add-expense/select-emoji/select-emoji.component-module.ts
+++ src/app/add-expense/select-emoji/select-emoji.component-module.ts
@@ -1,4 +1,5 @@
 import { ButtonComponent } from '../button/button.component';
+import { DisplayerComponent } from './displayer/displayer.component';
 import { HeadbarComponent } from '../headbar/headbar.component';
 import { NgModule } from '@angular/core';
 import { SelectEmojiComponent } from './select-emoji.component';
@@ -6,7 +7,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { KeyboardComponent } from './keyboard/keyboard.component';
 
 @NgModule({
-    declarations: [KeyboardComponent, SelectEmojiComponent],
+    declarations: [DisplayerComponent, KeyboardComponent, SelectEmojiComponent],
     imports: [ButtonComponent, HeadbarComponent, ReactiveFormsModule],
 })
 export class SelectEmojiComponentModule {}

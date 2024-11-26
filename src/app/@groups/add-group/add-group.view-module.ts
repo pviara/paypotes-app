685496fcad39a9dae762162a6837e138e7e56746
Pierre Viara <pierre.viara@outlook.com>
2024-11-26 09:30:56 +0100

    add view

---
+++ src/app/@groups/add-group/add-group.view-module.ts
@@ -0,0 +1,9 @@
+import { AddGroupView } from '@groups/add-group/add-group.view';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+    declarations: [AddGroupView],
+    imports: [RouterModule],
+})
+export class AddGroupViewModule {}
---
+++ src/app/@groups/add-group/add-group.view.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-add-group',
+    templateUrl: './add-group.view.html',
+    styleUrls: ['./add-group.view.scss'],
+})
+export class AddGroupView {}

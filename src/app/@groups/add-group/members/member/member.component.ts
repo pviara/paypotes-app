98974c3fda170c2621c6c921676982bc2f3de9cf
Pierre Viara <pierre.viara@outlook.com>
2024-11-28 17:44:39 +0100

    add single member component

---
+++ src/app/@groups/add-group/members/member/member.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'member',
+    templateUrl: './member.component.html',
+    styleUrls: ['./member.component.scss'],
+})
+export class MemberComponent {}
--- src/app/@groups/add-group/members/members.component-module.ts
+++ src/app/@groups/add-group/members/members.component-module.ts
@@ -1,10 +1,11 @@
 import { ButtonComponent } from '@shared/button/button.component';
 import { HeadbarComponent } from '@shared/headbar/headbar.component';
+import { MemberComponent } from '@groups/add-group/members/member/member.component';
 import { MembersComponent } from './members.component';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-    declarations: [MembersComponent],
+    declarations: [MemberComponent, MembersComponent],
     imports: [ButtonComponent, HeadbarComponent, ButtonComponent],
 })
 export class MembersComponentModule {}

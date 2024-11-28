9645c2c11c32a290cac166a657a6a257135ba009
Pierre Viara <pierre.viara@outlook.com>
2024-11-28 17:20:12 +0100

    use native assets alias for images

--- src/app/@groups/add-group/members/members.component-module.ts
+++ src/app/@groups/add-group/members/members.component-module.ts
@@ -1,9 +1,10 @@
+import { ButtonComponent } from '@shared/button/button.component';
 import { HeadbarComponent } from '@shared/headbar/headbar.component';
 import { MembersComponent } from './members.component';
 import { NgModule } from '@angular/core';
 
 @NgModule({
     declarations: [MembersComponent],
-    imports: [HeadbarComponent],
+    imports: [ButtonComponent, HeadbarComponent, ButtonComponent],
 })
 export class MembersComponentModule {}

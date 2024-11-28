498f79ccf882ecd08e3ca278be1ff7d5ced56f6c
Pierre Viara <pierre.viara@outlook.com>
2024-11-28 21:23:24 +0100

    add constraint

--- src/app/@expenses/add-expense/form.spec.ts
+++ src/app/@expenses/add-expense/form.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import {
     Form,
     InvalidLabelError,
@@ -54,7 +54,7 @@ describe('Form', () => {
             'should add a new field given valid label "%s" and value "%s"',
             (label, value) => {
                 sut.addField({ label, value });
-                expect(sut.get(label)).toBe(value);
+                expect(sut.get(label)?.value).toBe(value);
             },
         );
     });
@@ -81,12 +81,56 @@ describe('Form', () => {
         ])('should update field "%s" value to "%s"', (label, value) => {
             sut.addField({ label });
 
-            const initialValue = sut.get(label);
+            const initialValue = sut.get(label)?.value;
             sut.setField({ label, value });
-            const newValue = sut.get(label);
+            const newValue = sut.get(label)?.value;
 
             expect(initialValue).not.toBe(newValue);
             expect(newValue).toBe(value);
         });
+    });
+
+    describe('checking whether a field is valid', () => {
+        it('should throw an error when given field does not exist', () => {
+            expect(() => sut.valid('notExistingLabel')).toThrow();
+        });
+
+        it('should not throw an error when given field exists', () => {
+            const label = 'lastname';
+            sut.addField({ label });
+            expect(() => sut.valid(label)).not.toThrow();
+        });
+
+        it.each(['firstname', 'lastname', 'birthDate', 'age', 'isCool'])(
+            'should always return true when no constraint has been given',
+            (label) => {
+                sut.addField({ label });
+                expect(sut.valid(label)).toBe(true);
+            },
+        );
+
+        const onlyNumericWithOptionalDecimals = /^\d+(\.\d{1,2})?$/;
+        const onlyTenDigits = /^0[1-9]\d{8}$/;
+        const onlyAlphabeticCharacters = /^[a-zA-Z]+$/;
+
+        it.each([
+            ['firstname', 'T est', onlyAlphabeticCharacters],
+            ['firstname', 'invalid123', onlyAlphabeticCharacters],
+            ['firstname', '', onlyAlphabeticCharacters],
+            ['firstname', ' ', onlyAlphabeticCharacters],
+            ['telephone', '+33612345678', onlyTenDigits],
+            ['telephone', '124', onlyTenDigits],
+            ['telephone', '', onlyTenDigits],
+            ['amount', NaN, onlyNumericWithOptionalDecimals],
+            ['amount', 'test', onlyNumericWithOptionalDecimals],
+            ['amount', 'invalid', onlyNumericWithOptionalDecimals],
+            ['amount', ' ', onlyNumericWithOptionalDecimals],
+        ])(
+            'should return false when given constraint is not fulfilled',
+            (label, value, constraint) => {
+                sut.addField({ label, value, constraint });
+                expect(sut.valid(label)).toBe(false);
+            },
+        );
     });
 });
--- src/app/@expenses/add-expense/form.ts
+++ src/app/@expenses/add-expense/form.ts
@@ -1,26 +1,38 @@
-type FieldInput = { label: string; value?: unknown };
+type FieldInput = {
+    label: string;
+    value?: unknown;
+    constraint?: RegExp;
+};
 
 export class Form {
-    private fields = new Map<string, unknown>();
+    private fields = new Map<FieldInput['label'], FieldInput>();
 
-    get(label: string): unknown {
-        return this.fields.get(label);
+    get(label: string): FieldInput | null {
+        return this.fields.get(label) ?? null;
     }
 
     getFieldLabels(): Array<string> {
         return Array.from(this.fields.entries()).map(([key]) => key);
     }
 
-    addField({ label, value }: FieldInput): void {
+    addField({ label, value, constraint }: FieldInput): void {
         this.throwIfInvalid(label);
         this.throwIfExists(label);
-        this.fields.set(label, value);
+        this.fields.set(label, { label, value, constraint });
     }
 
     setField({ label, value }: FieldInput): void {
         this.throwIfNotFound(label);
-        this.fields.set(label, value);
+        this.fields.set(label, { label, value });
         return;
+    }
+
+    valid(label: string): any {
+        const field = this.fields.get(label);
+        if (!field) throw new LabelNotFoundError(label);
+
+        const { value, constraint } = field;
+        return constraint ? constraint.test(`${value}`) : true;
     }
 
     private throwIfInvalid(label: string): void {

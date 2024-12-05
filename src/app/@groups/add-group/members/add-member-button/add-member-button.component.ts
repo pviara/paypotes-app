3bc144a2145939b84138e7fbe74bba84f327aa5c
Pierre Viara <pierre.viara@outlook.com>
2024-12-05 12:35:09 +0100

    add add member button component

---
+++ src/app/@groups/add-group/members/add-member-button/add-member-button.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'add-member-button',
+    templateUrl: './add-member-button.component.html',
+    styleUrls: ['./add-member-button.component.scss'],
+})
+export class AddMemberButtonComponent {}
--- src/app/@groups/add-group/members/members.component-module.ts
+++ src/app/@groups/add-group/members/members.component-module.ts
@@ -1,11 +1,13 @@
+import { AddMemberButtonComponent } from '@groups/add-group/members/add-member-button/add-member-button.component';
 import { ButtonComponent } from '@shared/components/button/button.component';
 import { HeadbarComponent } from '@shared/components/headbar/headbar.component';
 import { MemberComponent } from '@groups/add-group/members/member/member.component';
 import { MembersComponent } from './members.component';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-    declarations: [MemberComponent, MembersComponent],
-    imports: [ButtonComponent, HeadbarComponent, ButtonComponent],
+    declarations: [AddMemberButtonComponent, MemberComponent, MembersComponent],
+    imports: [ButtonComponent, HeadbarComponent, RouterModule],
 })
 export class MembersComponentModule {}

e5b39dbde9b657382612cfd2225ffa09c7f8b0b4
Pierre Viara <pierre.viara@outlook.com>
2024-12-05 21:43:28 +0100

    edit route

--- src/app/@home/home.view-module.ts
+++ src/app/@home/home.view-module.ts
@@ -4,9 +4,15 @@ import { GroupsComponentModule } from '@home/groups/groups.component-module';
 import { HeadbarComponent } from '@home/headbar/headbar.component';
 import { HomeView } from '@home/home.view';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [HeadbarComponent, HomeView],
-    imports: [BalanceComponent, ExpensesComponentModule, GroupsComponentModule],
+    imports: [
+        BalanceComponent,
+        ExpensesComponentModule,
+        GroupsComponentModule,
+        RouterModule,
+    ],
 })
 export class HomeViewModule {}

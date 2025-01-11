25bacd62fe83cd55040a5d2d1c92fb5e16d6193d
Pierre Viara <pierre.viara@outlook.com>
2025-01-11 23:51:16 +0100

    add create button in group view

--- src/app/@groups/group/group.component.ts
+++ src/app/@groups/group/group.component.ts
@@ -5,6 +5,7 @@ import { ExpenseServiceToken } from '@core/services/expense/expense.service.prov
 import { FiltersEvent } from '@core/model/filters/filters-event';
 import { GroupServiceToken } from '@core/services/group/group.service.provider';
 import { ListElements } from '@core/model/list-element/list-element';
+import { Action } from '@shared/components/headbar/action-button/action-button.component';
 
 @Component({
     selector: 'group',
@@ -17,6 +18,8 @@ export class GroupComponent {
     private route = inject(ActivatedRoute);
 
     private groupId = '';
+
+    action: Action = { char: '+', route: '/groups/add-expense' };
 
     $group = this.route.params.pipe(
         tap((params) => (this.groupId = params['groupId'])),

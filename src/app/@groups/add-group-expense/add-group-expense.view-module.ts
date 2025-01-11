416e09007f135e0a7ca960077786caa3634d6b11
Pierre Viara <pierre.viara@outlook.com>
2025-01-12 00:03:40 +0100

    add group expense view

--- src/app/@groups/add-group-expense/add-group-expense.view-module.ts
+++ src/app/@groups/add-group-expense/add-group-expense.view-module.ts
@@ -1,4 +1,10 @@
+import { addGroupExpenseRoutes } from '@groups/add-group-expense/add-group-expense.routes';
+import { AddGroupExpenseView } from '@groups/add-group-expense/add-group-expense.view';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-@NgModule({})
+@NgModule({
+    declarations: [AddGroupExpenseView],
+    imports: [RouterModule.forChild(addGroupExpenseRoutes)],
+})
 export class AddGroupExpenseViewModule {}
---
+++ src/app/@groups/add-group-expense/add-group-expense.view.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './add-group-expense.view.html',
+})
+export class AddGroupExpenseView {}

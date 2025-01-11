16366314d4bd6d3262f296601f218b82b0e89e37
Pierre Viara <pierre.viara@outlook.com>
2025-01-11 23:56:34 +0100

    add aliases in file path in module

--- src/app/@expenses/add-expense/add-expense.view-module.ts
+++ src/app/@expenses/add-expense/add-expense.view-module.ts
@@ -1,13 +1,13 @@
-import { addExpenseRoutes } from './add-expense.routes';
-import { AddExpenseView } from './add-expense.view';
-import { ChoseContactComponentModule } from './chose-contact/chose-contact.component-module';
-import { FillDetailsComponentModule } from './fill-details/fill-details.component-module';
+import { addExpenseRoutes } from '@expenses/add-expense/add-expense.routes';
+import { AddExpenseView } from '@expenses/add-expense/add-expense.view';
+import { ChoseContactComponentModule } from '@expenses/add-expense/chose-contact/chose-contact.component-module';
+import { FillDetailsComponentModule } from '@expenses/add-expense/fill-details/fill-details.component-module';
 import { NgModule } from '@angular/core';
-import { ReadSummaryComponentModule } from './read-summary/read-summary.component-module';
+import { ReadSummaryComponentModule } from '@expenses/add-expense/read-summary/read-summary.component-module';
 import { RouterModule } from '@angular/router';
-import { SelectEmojiComponentModule } from './select-emoji/select-emoji.component-module';
-import { SelectPersonComponentModule } from './select-person/select-person.component-module';
-import { SetBalanceComponentModule } from './set-balance/set-balance.component-module';
+import { SelectEmojiComponentModule } from '@expenses/add-expense/select-emoji/select-emoji.component-module';
+import { SelectPersonComponentModule } from '@expenses/add-expense/select-person/select-person.component-module';
+import { SetBalanceComponentModule } from '@expenses/add-expense/set-balance/set-balance.component-module';
 import { ServicesModule } from '@core/services/services.module';
 
 @NgModule({

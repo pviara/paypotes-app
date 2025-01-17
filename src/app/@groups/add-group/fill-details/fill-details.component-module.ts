518354d49b5703e931673ee1673cd41c5b1e0845
Pierre Viara <pierre.viara@outlook.com>
2025-01-17 16:30:40 +0100

    edit import path

--- src/app/@groups/add-group/fill-details/fill-details.component-module.ts
+++ src/app/@groups/add-group/fill-details/fill-details.component-module.ts
@@ -1,6 +1,6 @@
 import { ButtonComponent } from '@shared/components/button/button.component';
 import { CommonModule } from '@angular/common';
-import { FillDetailsComponent } from '@expenses/add-expense/fill-details/fill-details.component';
+import { FillDetailsComponent } from '@groups/add-group/fill-details/fill-details.component';
 import { HeadbarComponent } from '@shared/components/headbar/headbar.component';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';

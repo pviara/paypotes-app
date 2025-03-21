e1e10a520ed7eff135df8d4de8a0543dc3f77efc
Pierre Viara <pierre.viara@outlook.com>
2025-03-21 10:35:26 +0100

    🐞 fix scroll in contacts expense form (#96)

--- src/app/shared/components/selector-form/selector-form.component.ts
+++ src/app/shared/components/selector-form/selector-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, inject, input, Output } from '@angular/core';
+import { Component, EventEmitter, input, Output } from '@angular/core';
 import { Contact } from '@core/model/contact/contact';
 import { User } from '@core/model/user/user';
 
@@ -8,7 +8,7 @@ import { User } from '@core/model/user/user';
     styleUrls: ['./selector-form.component.scss'],
 })
 export class SelectorFormComponent {
-    persons = input.required<Contact[] | User[]>();
+    persons = input<Contact[] | User[] | null>(null);
 
     @Output()
     personSelected = new EventEmitter<Contact | User>();

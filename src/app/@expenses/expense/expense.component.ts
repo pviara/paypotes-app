d34c124f9e0e3ab72670e808ca198141a09dc6cb
Pierre Viara <pierre.viara@outlook.com>
2025-04-07 22:00:02 +0200

    fix: reorder import statements in expense component

--- src/app/@expenses/expense/expense.component.ts
+++ src/app/@expenses/expense/expense.component.ts
@@ -1,8 +1,8 @@
 import { ActivatedRoute, Router } from '@angular/router';
 import { Component, inject } from '@angular/core';
 import { ExpenseServiceToken } from '@core/services/expense/expense.service.provider';
-import { NotificationService } from '@core/services/notification/notification.service';
 import { map, shareReplay, switchMap, tap } from 'rxjs';
+import { NotificationService } from '@core/services/notification/notification.service';
 
 @Component({
     selector: 'expense',

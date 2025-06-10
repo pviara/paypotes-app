72998c9ffdb10d1af423049780d3a02aac7479ba
Pierre Viara <pierre.viara@outlook.com>
2025-06-10 17:36:06 +0200

    remove unused component

--- src/app/@groups/add-group/chose-contacts/chose-contacts.component-module.ts
+++ src/app/@groups/add-group/chose-contacts/chose-contacts.component-module.ts
@@ -3,20 +3,17 @@ import { ButtonComponent } from '@shared/components/button/button.component';
 import { ChoseContactsComponent } from '@groups/add-group/chose-contacts/chose-contacts.component';
 import { ChosePersonsFormComponentModule } from '@shared/components/chose-persons-form/chose-persons-form.component-module';
 import { CommonModule } from '@angular/common';
-import { ContactComponent } from '@groups/add-group/chose-contacts/contact/contact.component';
 import { FormService } from '@core/services/form/form.service';
 import { HeadbarComponent } from '@shared/components/headbar/headbar.component';
 import { NgModule } from '@angular/core';
-import { SelectorFormComponentModule } from '@shared/components/selector-form/selector-form.component-module';
 
 @NgModule({
-    declarations: [ChoseContactsComponent, ContactComponent],
+    declarations: [ChoseContactsComponent],
     imports: [
         ButtonComponent,
         ChosePersonsFormComponentModule,
         CommonModule,
         HeadbarComponent,
-        SelectorFormComponentModule,
     ],
 })
 export class ChoseContactsComponentModule {

a19e16057a0e191129b494afd1b65458d22097b2
Pierre Viara <pierre.viara@outlook.com>
2025-06-16 18:34:00 +0200

    add delay on http client

--- src/app/core/services/http-client/default-http-client.service.ts
+++ src/app/core/services/http-client/default-http-client.service.ts
@@ -1,19 +1,29 @@
+import { isPlatformBrowser } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { HttpClientService } from '@core/services/http-client/http-client.service';
-import { Observable } from 'rxjs';
+import { delay, Observable } from 'rxjs';
 
 export class DefaultHttpClientService implements HttpClientService {
+    private platformId = inject(PLATFORM_ID);
+
     constructor(private httpClient: HttpClient) {}
 
     get<T>(url: string): Observable<T> {
-        return this.httpClient.get<T>(url);
+        return this.httpClient
+            .get<T>(url)
+            .pipe(delay(isPlatformBrowser(this.platformId) ? 1000 : 0));
     }
 
     put(url: string, payload: unknown): Observable<void> {
-        return this.httpClient.put<void>(url, payload);
+        return this.httpClient
+            .put<void>(url, payload)
+            .pipe(delay(isPlatformBrowser(this.platformId) ? 1000 : 0));
     }
 
     post(url: string, payload: unknown): Observable<void> {
-        return this.httpClient.post<void>(url, payload);
+        return this.httpClient
+            .post<void>(url, payload)
+            .pipe(delay(isPlatformBrowser(this.platformId) ? 1000 : 0));
     }
 }

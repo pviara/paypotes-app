5abb3ab50797ef6e8bddb3edb02b7b10e7b02f3c
Pierre Viara <pierre.viara@outlook.com>
2025-06-26 19:29:38 +0200

    🐞 fix form redirecting although user could not be found (#122)

--- src/app/shared/components/person-form/manual-name-input/manual-name-input.component.ts
+++ src/app/shared/components/person-form/manual-name-input/manual-name-input.component.ts
@@ -1,4 +1,4 @@
-import { BehaviorSubject, debounceTime, tap } from 'rxjs';
+import { BehaviorSubject, debounceTime, distinctUntilChanged, tap } from 'rxjs';
 import {
     Component,
     EventEmitter,
@@ -17,8 +17,10 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class ManualNameInputComponent implements OnInit {
     private formBuilder = inject(FormBuilder);
 
+    error = input<string>();
+
     form = this.formBuilder.group({
-        name: this.formBuilder.control('', [
+        name: this.formBuilder.nonNullable.control('', [
             Validators.required,
             Validators.minLength(2),
         ]),
@@ -26,23 +28,31 @@ export class ManualNameInputComponent implements OnInit {
 
     $isLoading = input<BehaviorSubject<boolean> | null>(null);
 
-    error = input<string>();
-
     @Output()
     searching = new EventEmitter<string>();
 
     ngOnInit(): void {
         this.form.valueChanges
-            .pipe(debounceTime(1000))
-            .subscribe(({ name }) => {
-                if (!this.$isLoading()?.getValue() && name) {
-                    this.searching.emit(name);
-                }
-            });
+            .pipe(
+                debounceTime(700),
+                distinctUntilChanged(
+                    (previous, current) => previous.name === current.name,
+                ),
+                tap(({ name }) => {
+                    if (!this.$isLoading()?.getValue() && name) {
+                        this.searching.emit(name);
+                    }
+                }),
+            )
+            .subscribe();
 
-        this.$isLoading()?.subscribe((isLoading) => {
-            if (isLoading) this.form.controls.name.disable();
-            else this.form.controls.name.enable();
-        });
+        this.$isLoading()
+            ?.pipe(
+                tap((isLoading) => {
+                    if (isLoading) this.form.controls.name.disable();
+                    else this.form.controls.name.enable();
+                }),
+            )
+            .subscribe();
     }
 }

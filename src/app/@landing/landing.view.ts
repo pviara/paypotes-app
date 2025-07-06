39663b0f1067ca0329be524362845f2ee6f9146a
Pierre Viara <pierre.viara@outlook.com>
2025-07-06 23:53:53 +0200

    add glowing

--- src/app/@landing/landing.view.ts
+++ src/app/@landing/landing.view.ts
@@ -1,27 +1,47 @@
-import { AfterViewInit, Component, PLATFORM_ID, inject } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import {
+    AfterViewInit,
+    Component,
+    OnInit,
+    PLATFORM_ID,
+    inject,
+} from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { environment } from 'src/environments/environment';
 import { Gradient } from 'whatamesh';
-import { isPlatformBrowser } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { version } from '../../../package.json';
+import { BehaviorSubject, delay, tap } from 'rxjs';
 
 @Component({
     selector: 'landing',
     templateUrl: './landing.view.html',
     styleUrls: ['./landing.view.scss'],
     standalone: true,
-    imports: [RouterModule],
+    imports: [CommonModule, RouterModule],
 })
-export class LandingView implements AfterViewInit {
+export class LandingView implements AfterViewInit, OnInit {
     private platformId = inject(PLATFORM_ID);
+    private route = inject(ActivatedRoute);
 
     readonly version = version;
+
+    $loading = new BehaviorSubject(true);
 
     ngAfterViewInit(): void {
         if (isPlatformBrowser(this.platformId)) {
             const gradient = new Gradient();
             gradient.initGradient('#gradient');
         }
+    }
+
+    ngOnInit(): void {
+        this.route.queryParams
+            .pipe(
+                tap((params) => {
+                    if (!params['token']) this.$loading.next(false);
+                }),
+            )
+            .subscribe();
     }
 
     getGoogleSignInLink(): string {

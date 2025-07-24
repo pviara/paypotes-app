4c38fbeebe6096a3043d54cbd1c540830d3914a6
Pierre Viara <pierre.viara@outlook.com>
2025-07-24 17:00:08 +0200

    edit package and keyboard conf

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,14 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'paypot.pviara.dev',
-  appName: 'paypot-app',
-  webDir: 'dist/paypot-app/browser'
+    appId: 'paypot.pviara.dev',
+    appName: 'paypot-app',
+    webDir: 'dist/paypot-app/browser',
+    plugins: {
+        Keyboard: {
+            resize: 'body',
+        },
+    },
 };
 
 export default config;

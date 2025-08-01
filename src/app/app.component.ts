10eb81cfa59d9e2e4b60041b52b66a1d1f3f10f2
Pierre Viara <pierre.viara@outlook.com>
2025-08-01 20:41:53 +0200

    lock screen orientation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { AppNotificationComponent } from 'src/app/notification/app-notification.component';
 import { AppMenuComponentModule } from 'src/app/menu/app-menu-component.module';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ContactsViewModule } from '@contacts/contacts.view-module';
 import { ExpensesViewModule } from '@expenses/expenses.view-module';
 import { HomeViewModule } from '@home/home.view-module';
 import { RouterOutlet } from '@angular/router';
+import { ScreenOrientation } from '@capacitor/screen-orientation';
 
 @Component({
     selector: 'app-root',
@@ -20,4 +21,8 @@ import { RouterOutlet } from '@angular/router';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+    async ngOnInit(): Promise<void> {
+        await ScreenOrientation.lock({ orientation: 'portrait' });
+    }
+}
